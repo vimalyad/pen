5d3652ca46a89b2d9eaf8c011ed48f7bc74ef5ec
Vimal290704 <vimalyadavkr001@gmail.com>
2026-03-24 16:01:16 +0530

    Added main class for testing

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,47 @@
 package Pen.src;
 
-public class Main {
-    static void main() {
+import Pen.src.factory.PenFactory;
+import Pen.src.pen.Pen;
 
+public class Main {
+    public static void main(String[] args) {
+
+        Pen ballPen = PenFactory.createPen("ball-pen", "blue", "click");
+        ballPen.open();
+        System.out.println("Current color: " + ballPen.getColor());
+        ballPen.write("Hi I am Ball Pen!");
+        ballPen.changeInkSource("red");
+        System.out.println("Current color: " + ballPen.getColor());
+        ballPen.write("Hi I am Ball Pen!");
+        ballPen.close();
+
+        System.out.println();
+
+        Pen gelPen = PenFactory.createPen("gel-pen", "black", "cap");
+        gelPen.open();
+        System.out.println("Current color: " + gelPen.getColor());
+        gelPen.write("HI I am Gel Pen!");
+        gelPen.close();
+
+        System.out.println();
+
+        Pen inkPen = PenFactory.createPen("ink-pen", "green", "cap");
+        inkPen.open();
+        System.out.println("Current color: " + inkPen.getColor());
+        inkPen.write("HI I am Ink Pen!");
+        inkPen.changeInkSource("purple");
+        System.out.println("Current color: " + inkPen.getColor());
+        inkPen.write("HI I am Ink Pen!");
+        inkPen.close();
+
+        System.out.println();
+
+        try {
+            Pen closedPen = PenFactory.createPen("ball-pen", "blue", "click");
+            closedPen.write("HI I am Ball Pen!");
+        } catch (IllegalStateException e) {
+            System.out.println("Caught expected error: " + e.getMessage());
+        }
     }
 }
+
